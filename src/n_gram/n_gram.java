dcf1dd37652fdf488fc78c303f56ef514a22275a
Hong-Jie Dai <hjdai@nttu.edu.tw>
2015-11-26 16:42:06 +0800

    修正 index 請加上注解

--- src/n_gram/n_gram.java
+++ src/n_gram/n_gram.java
@@ -28,26 +28,34 @@ public class n_gram {
 		}
 		return ngrams;
 	}
-	public static HashMap<String, Integer> index(
-			HashMap<String, Integer> index, int n, String str) {
-		// TODO 檢查有沒有 tab
-		int count=0;
+
+	public static long count;
+
+	/**
+	 * 
+	 * @param index
+	 * @param n_gram
+	 * @param str
+	 * @return
+	 */
+	public static HashMap<String, Long> index(HashMap<String, Long> index,
+			int n_gram, String str) {
+		// 檢查有沒有 tab
 		str = str.substring(str.indexOf("\t") + 1);
 		String[] words = str.split(" ");
-		for (int i = 0; i < words.length - n + 1; i++) {
-			String ngram = concat(words, i, i + n);
-			if (ngram.contains("	")) {
-				index.remove(ngram);
-				continue;
-
-			}
+		for (int i = 0; i < words.length - n_gram + 1; i++) {
+			String ngram = concat(words, i, i + n_gram);
+			// if (ngram.contains("	")) {
+			// index.remove(ngram);
+			// continue;
+			// }
 			if (!index.containsKey(ngram)) {
-				index.put(ngram, count+=1);
+				index.put(ngram, count += 1);
 			}
-			index.put(ngram, count);
 		}
 		return index;
 	}
+
 	public static String concat(String[] words, int start, int end) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = start; i < end; i++)
@@ -56,10 +64,12 @@ public class n_gram {
 	}
 
 	public static void main(String[] argv) throws IOException {
-		FileInputStream fr = new FileInputStream("Seg_TrainingData.txt");
-		BufferedReader br = new BufferedReader(new InputStreamReader(fr,"UTF-8"));
-		FileInputStream Index_fr = new FileInputStream("Seg_TrainingData.txt");
-		BufferedReader Index_br = new BufferedReader(new InputStreamReader(Index_fr,"UTF-8"));
+		FileInputStream fr = new FileInputStream("Seg_TestingData.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(fr,
+				"UTF-8"));
+		FileInputStream Index_fr = new FileInputStream("Seg_TestingData.txt");
+		BufferedReader Index_br = new BufferedReader(new InputStreamReader(
+				Index_fr, "UTF-8"));
 		String strNum = null;
 		String strNum_index = null;
 		String Ngram_line = null;
@@ -68,45 +78,45 @@ public class n_gram {
 		Path Index_file = null;
 		BufferedWriter bufferedWriter = null;
 		HashMap<String, Integer> ngrams = new HashMap<String, Integer>();
-		HashMap<String, Integer> index = new HashMap<String, Integer>();
+		HashMap<String, Long> index = new HashMap<String, Long>();
 
 		while ((strNum = br.readLine()) != null) {
-			//ngrams = n_gram.ngrams(ngrams, 1, strNum);
-			 ngrams = n_gram.ngrams(ngrams, 2, strNum);
+			// ngrams = n_gram.ngrams(ngrams, 1, strNum);
+			ngrams = n_gram.ngrams(ngrams, 2, strNum);
 		}
 		while ((strNum_index = Index_br.readLine()) != null) {
-			//index = n_gram.index(index, 1, strNum_index);
-			 index = n_gram.index(index, 2, strNum_index);
+			// index = n_gram.index(index, 1, strNum_index);
+			index = n_gram.index(index, 2, strNum_index);
 		}
 		try {
-			if (Files.exists(Paths.get("Ngram_data_output.txt")))
-			{
+			if (Files.exists(Paths.get("Ngram_data_output.txt"))) {
 				Files.delete(Paths.get("Ngram_data_output.txt"));
 			}
 			Ngram_file = Files.createFile(Paths.get("Ngram_data_output.txt"));
-			bufferedWriter = Files.newBufferedWriter(Ngram_file,Charset.forName("UTF-8"));
-				for (Entry<String, Integer> m : ngrams.entrySet()) {
-					Ngram_line = (m.getKey() + " " + m.getValue());
-					bufferedWriter.write(Ngram_line);
-					bufferedWriter.newLine();
-				}
-				bufferedWriter.close();
+			bufferedWriter = Files.newBufferedWriter(Ngram_file,
+					Charset.forName("UTF-8"));
+			for (Entry<String, Integer> m : ngrams.entrySet()) {
+				Ngram_line = (m.getKey() + " " + m.getValue());
+				bufferedWriter.write(Ngram_line);
+				bufferedWriter.newLine();
+			}
+			bufferedWriter.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		try {
-			if (Files.exists(Paths.get("Index_data_output.txt")))
-			{
+			if (Files.exists(Paths.get("Index_data_output.txt"))) {
 				Files.delete(Paths.get("Index_data_output.txt"));
 			}
 			Index_file = Files.createFile(Paths.get("Index_data_output.txt"));
-			bufferedWriter = Files.newBufferedWriter(Index_file,Charset.forName("UTF-8"));
-				for (Entry<String, Integer> n : index.entrySet()) {
-					Index_line = (n.getKey() + ":" + n.getValue());
-					bufferedWriter.write(Index_line);
-					bufferedWriter.newLine();
-				}
-				bufferedWriter.close();
+			bufferedWriter = Files.newBufferedWriter(Index_file,
+					Charset.forName("UTF-8"));
+			for (Entry<String, Long> n : index.entrySet()) {
+				Index_line = (n.getKey() + ":" + n.getValue());
+				bufferedWriter.write(Index_line);
+				bufferedWriter.newLine();
+			}
+			bufferedWriter.close();
 		} catch (IOException x) {
 			x.printStackTrace();
 		}
